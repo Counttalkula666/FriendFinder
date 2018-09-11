4027102f06088c5640e6c4e716376f7eba6c117d
Counttalkula666 <elenabelgum@gmail.com>
2018-09-11 08:23:15 -0700

    updated html, need friends and questions

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
+<!-- README -->
+<!-- FLESH THIS POUT AND INCLUDE SCREENSHOTS AND LINKS -->
 # FriendFinder
 
 Compatibility based dating app using HTML, Javascript, Bootstrap, Express, heroku, etc.
 
-Simply complete the survey to find the best match for you based on the survey question answers.
+Simply complete the survey to find the best match for you from the database based on the survey question answers.
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,24 +1,34 @@
+//FRIENDS DATA
+
 //FINISH CREATING FRIENDS
-// 5. You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
+// 5. You should save your application's data inside of `app/data/friends.js` as an array of objects.
 
-var friends = 
-{
-  name: "friendname",
-  photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  scores:[
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
-    ]
-  name: "dufus"
-  photo:
-  scores:
+var friends = [
+	{
+		name: "",
+		photo: "",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "",
+		photo: "",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "",
+		photo: "",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "",
+		photo: "",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "",
+		photo: "",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
 
-};
+module.exports = friends;
