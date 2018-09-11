5e7a3b1f2f64881fc5a2267901215723c35de315
Counttalkula666 <elenabelgum@gmail.com>
2018-09-11 07:30:10 -0700

    home html added, page working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,21 +1,20 @@
-var path = require('path');
-
+var path = require("path");
 
 // Your `apiRoutes.js` file should contain two routes:
 
 // * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
-// * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
+// * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
 // 6. Determine the user's most compatible friend using the following as a guide:
 
 //    * Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
-//    * With that done, compare the difference WITH A LOOP between current user's scores against those from other users, question by question. 
+//    * With that done, compare the difference WITH A LOOP between current user's scores against those from other users, question by question.
 //Add up the differences to calculate the `totalDifference`.
-//      * Example: 
+//      * Example:
 //        * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
 //        * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
 //        * Total Difference: **2 + 1 + 2 =** **_5_**
-//    * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on. 
+//    * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on.
 //    * The closest match will be the user with the least amount of difference.
 
 var friends = require("../data/friends");
@@ -29,21 +28,21 @@ module.exports = function(app) {
     console.log(req.body.scores);
     var user = req.body;
 
-    for(var i = 0; i < user.scores.length; i++) {
+    for (var i = 0; i < user.scores.length; i++) {
       user.scores[i] = parseInt(user.scores[i]);
     }
 
     var bestFriendIndex = 0;
     var minimumDifference = 40;
 
-    for(var i = 0; i < friends.length; i++) {
+    for (var i = 0; i < friends.length; i++) {
       var totalDifference = 0;
-      for(var j = 0; j < friends[i].scores.length; j++) {
+      for (var j = 0; j < friends[i].scores.length; j++) {
         var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
         totalDifference += difference;
       }
 
-      if(totalDifference < minimumDifference) {
+      if (totalDifference < minimumDifference) {
         bestFriendIndex = i;
         minimumDifference = totalDifference;
       }
