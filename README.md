afc5f54ef1ea6c156c594cc6f6714b2179283b2e
Counttalkula666 <elenabelgum@gmail.com>
2018-09-11 07:51:07 -0700

    update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FriendFinder
-Compatibility based dating app
+
+Compatibility based dating app using HTML, Javascript, Bootstrap, Express, heroku, etc.
+
+Simply complete the survey to find the best match for you based on the survey question answers.
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,33 +1,23 @@
+//API ROUTES
+//DEPENDENCIES
 var path = require("path");
+var friends = require("../data/friends");
 
 // Your `apiRoutes.js` file should contain two routes:
-
-// * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
-// * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-
-// 6. Determine the user's most compatible friend using the following as a guide:
-
-//    * Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
-//    * With that done, compare the difference WITH A LOOP between current user's scores against those from other users, question by question.
-//Add up the differences to calculate the `totalDifference`.
-//      * Example:
-//        * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
-//        * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
-//        * Total Difference: **2 + 1 + 2 =** **_5_**
-//    * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on.
-//    * The closest match will be the user with the least amount of difference.
-
-var friends = require("../data/friends");
+// * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends. 
 // Return friends as JSON
 module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
-
+// * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
   app.post("/api/friends", function(req, res) {
     console.log(req.body.scores);
     var user = req.body;
 
+// 6. Determine the user's most compatible friend using the following as a guide:
+//    * Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
+//    * With that done, compare the difference WITH A LOOP between current user's scores against those from other users, question by question.
     for (var i = 0; i < user.scores.length; i++) {
       user.scores[i] = parseInt(user.scores[i]);
     }
@@ -35,6 +25,13 @@ module.exports = function(app) {
     var bestFriendIndex = 0;
     var minimumDifference = 40;
 
+//Add up the differences to calculate the `totalDifference`.
+//      * Example:
+//        * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
+//        * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
+//        * Total Difference: **2 + 1 + 2 =** **_5_**
+//    * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on.
+//    * The closest match will be the user with the least amount of difference.
     for (var i = 0; i < friends.length; i++) {
       var totalDifference = 0;
       for (var j = 0; j < friends[i].scores.length; j++) {
@@ -47,7 +44,7 @@ module.exports = function(app) {
         minimumDifference = totalDifference;
       }
     }
-
+//ADDING FRIENDS TO THE DATABASE
     friends.push(user);
 
     res.json(friends[bestFriendIndex]);
