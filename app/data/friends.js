90eea3df4c45875881123190ebd4703f1f4c69cb
Counttalkula666 <elenabelgum@gmail.com>
2018-09-12 20:27:08 -0700

    need questions and to exclude the user from getting themselves as a result

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,28 +5,28 @@
 
 var friends = [
 	{
-		name: "",
-		photo: "",
+		name: "Fred",
+		photo: "./app/assets/Fred.jpg",
 		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
 	},
 	{
-		name: "",
-		photo: "",
+		name: "Bob",
+		photo: "./app/assets/Bob.jpg",
 		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
 	},
 	{
-		name: "",
-		photo: "",
+		name: "Mary",
+		photo: "./app/assets/Mary.jpg",
 		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
 	},
 	{
-		name: "",
-		photo: "",
+		name: "Diane",
+		photo: "./app/assets/Diane.jpg",
 		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
 	},
 	{
-		name: "",
-		photo: "",
+		name: "Pat",
+		photo: "./app/assets/Pat.jpg",
 		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
 	}
 ];
