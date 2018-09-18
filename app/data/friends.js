03fe22ce9e3e20c9ec4e67e85d7897b7ccaa8ba5
Counttalkula666 <elenabelgum@gmail.com>
2018-09-17 19:58:48 -0700

    almost done

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,32 +1,31 @@
 //FRIENDS DATA
 
-//FINISH CREATING FRIENDS
 // 5. You should save your application's data inside of `app/data/friends.js` as an array of objects.
 
 var friends = [
 	{
 		name: "Fred",
-		photo: "./app/assets/Fred.jpg",
+		photo: "/assets/Fred.jpg",
 		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
 	},
 	{
 		name: "Bob",
-		photo: "./app/assets/Bob.jpg",
+		photo: "/assets/Bob.jpg",
 		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
 	},
 	{
 		name: "Mary",
-		photo: "./app/assets/Mary.jpg",
+		photo: "/assets/Mary.jpg",
 		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
 	},
 	{
 		name: "Diane",
-		photo: "./app/assets/Diane.jpg",
+		photo: "/assets/Diane.jpg",
 		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
 	},
 	{
 		name: "Pat",
-		photo: "./app/assets/Pat.jpg",
+		photo: "/assets/Pat.jpg",
 		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
 	}
 ];
